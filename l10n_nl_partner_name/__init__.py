2b0242db48784d9167e03375617b00cd5460bebb
Bastian Guenther <Bastian.Guenther@ametras.com>
2023-09-27 12:09:32 +0200

    [FIX] l10n_nl_partner_name: Fixed error, when config parameter already exists

--- l10n_nl_partner_name/__init__.py
+++ l10n_nl_partner_name/__init__.py
@@ -1,2 +1,11 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from . import model
+
+from odoo import SUPERUSER_ID
+from odoo.api import Environment
+
+
+def _post_init(cr, _):
+    env = Environment(cr, SUPERUSER_ID, {})
+    # Here we define to clear connector logs after 730 hours by default
+    env["ir.config_parameter"].sudo().set_param("partner_names_order", "first_last")
